c490b29cdabbfc33e34d00597af79dd9120a5a8f
CouchPtato <singhsomay467@gmail.com>
2025-11-26 00:59:42 +0530

    Kruskals practise

--- Advanced Algorithm/Practise/Practicals.cpp
+++ Advanced Algorithm/Practise/Practicals.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int comparisons = 0;
+// int comparisons = 0;
 
-int partition(vector<int> &Array, int start, int end)
-{
-    int x = Array[end];
-    int i = start - 1;
+// int partition(vector<int> &Array, int start, int end)
+// {
+//     int x = Array[end];
+//     int i = start - 1;
 
-    for (int j = start; j <= end - 1; j++)
-    {
-        comparisons++;
-        if (Array[j] <= x)
-        {
-            i = i + 1;
-            swap(Array[i], Array[j]);
-        }
-    }
-    swap(Array[i+1], Array[end]);
-    return (i+1);
-}
+//     for (int j = start; j <= end - 1; j++)
+//     {
+//         comparisons++;
+//         if (Array[j] <= x)
+//         {
+//             i = i + 1;
+//             swap(Array[i], Array[j]);
+//         }
+//     }
+//     swap(Array[i+1], Array[end]);
+//     return (i+1);
+// }
 
-int randomizedPartition(vector<int> &Array, int start, int end)
-{
-    int j = start + rand() % (end - start + 1);
-    swap(Array[end], Array[j]);
-    return partition(Array, start, end);
-}
+// int randomizedPartition(vector<int> &Array, int start, int end)
+// {
+//     int j = start + rand() % (end - start + 1);
+//     swap(Array[end], Array[j]);
+//     return partition(Array, start, end);
+// }
 
 // void quickSort(vector<int> &A, int p, int r)
 // {
@@ -95,3 +95,99 @@ int randomizedPartition(vector<int> &Array, int start, int end)
 
 //     return 0;
 // }
+
+struct Edge
+{
+    int u, v, w;
+};
+
+struct DSU
+{
+    vector<int> parent, rank;
+
+    DSU (int n)
+    {
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for(int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x)
+    {
+        if(parent[x] == x)
+        {
+            return x;
+        }
+        return parent[x] = find(parent[x]); // path compression
+    }
+
+    void unite(int x, int y)
+    {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+
+        if(rank[x] < rank[y])
+            parent[x] = y;
+        else if(rank[y] < rank[x])
+            parent[y] = x;
+        else
+        {
+            parent[y] = x;
+            rank[x]++;
+        }
+    }
+};
+
+int main() 
+{
+    int V, E;
+
+    cout << "Enter number of vertices: ";
+    cin >> V;
+    cout << "Enter number of edges: ";
+    cin >> E;
+
+    vector<Edge> edges(E);
+
+    cout << "Enter edges (u v w) with vertices numbered 0 to " << V - 1 << ":\n";
+
+    for (int i = 0; i < E; i++) 
+    {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+    }
+
+    // Sort edges by weight
+    sort(edges.begin(), edges.end(), [](const Edge &a, const Edge &b) 
+    {
+        return a.w < b.w;
+    });
+
+    DSU dsu(V);
+    int mstWeight = 0;
+    vector<Edge> mstEdges;
+
+    for (auto &e : edges) 
+    {
+        if (dsu.find(e.u) != dsu.find(e.v)) 
+        {
+            dsu.unite(e.u, e.v);
+            mstEdges.push_back(e);
+            mstWeight += e.w;
+        }
+    }
+
+    cout << "Edges in MST:\n";
+    for (auto &e : mstEdges) 
+    {
+        cout << e.u << " - " << e.v << " (weight " << e.w << ")\n";
+    }
+    cout << "Total weight of MST: " << mstWeight << "\n";
+
+    return 0;
+}
